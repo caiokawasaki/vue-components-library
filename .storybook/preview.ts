53571e084becfc128bdb5aac65f4fe73b0509ec8
caiokawasaki <caio@rilo.com.br>
2025-08-28 02:13:04 -0300

    Sort Storybook stories for better organization through custom `storySort` function.

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -9,6 +9,20 @@ const preview: Preview = {
                 color: /(background|color)$/i,
                 date: /Date$/i
             }
+        },
+        options: {
+            storySort: (a, b) => {
+                const aIsDocs = a.type === 'docs'
+                const bIsDocs = b.type === 'docs'
+
+                if (aIsDocs !== bIsDocs) return aIsDocs ? -1 : 1
+
+                if (aIsDocs && bIsDocs) {
+                    return a.title.localeCompare(b.title, undefined, {numeric: true})
+                }
+
+                return a.id === b.id ? 0 : a.id.localeCompare(b.id, undefined, {numeric: true})
+            }
         }
     }
 }
